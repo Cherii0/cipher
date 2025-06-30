0918757df153c33f36d6b23b8670b94659d465a8
Cherii0 <zygmunt.latyszewicz@outlook.com>
2025-06-30 10:05:19 +0200

    fix bug with letter a

--- scripts/cipher.py
+++ scripts/cipher.py
@@ -120,8 +120,17 @@ class CipherAlgorithm:
         latin_end = 26
 
         latin_codes = [c for c in range(latin_bgn, latin_end)]
+        # [0, 1, 2 ... 25] # 26 total = 26 latin letters
         latin_letters_dict = {char_: code_ for (char_, code_) in zip(string.ascii_lowercase, latin_codes)}
+        # {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11,
+        # 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22,
+        # 'x': 23, 'y': 24, 'z': 25}
         latin_codes_dict = {code_ : char_ for (char_, code_) in latin_letters_dict.items()}
+        # {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l'
+        # , 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w',
+        # 23: 'x', 24: 'y', 25: 'z'}
+
+
 
         non_cipher_content_codes = []
         for char_ in content:
@@ -132,19 +141,21 @@ class CipherAlgorithm:
 
         cipher_content_codes = []
         for code_ in non_cipher_content_codes:
-            if not code_:
-                cipher_content_codes.append(None)
-            elif code_ < self.rot13_offset:
+            if code_ < self.rot13_offset:
                 cipher_content_codes.append(code_ + self.rot13_offset)
             else:
                 cipher_content_codes.append(code_ - self.rot13_offset)
 
         cipher_content = []
         for code_ in cipher_content_codes:
+
             if not code_:
                 cipher_content.append("*")
             else:
                 cipher_content.append(latin_codes_dict[code_])
+
+        input(".")
+
 
         return "".join(cipher_content)
 
